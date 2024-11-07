f0c761074d9d2e5cf433490ef81ac197c9e9c202
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-11-07 14:31:44 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>
+        head
+    </h1>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        color: blue
+    }
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div>
+            <input type="text" v-model="userInput">
+            <button @click="search()">cerca</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+    export default {
+        data() {
+            return {
+                userInput : '',
+                pokemon: {},
+            }
+        },
+
+        methods: {
+            pkmnCall() {
+                axios.get('https://pokeapi.co/api/v2/pokemon/',{
+                    params: {
+                    }
+                }).then((res)=>{
+                    
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
