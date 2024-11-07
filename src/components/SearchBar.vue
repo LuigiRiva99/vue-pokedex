f65cd20873f9d45838634f3c08e65ffdbbaa1628
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-11-07 14:59:32 +0100

    ADD pokemon search axios call

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,7 +2,15 @@
     <div>
         <div>
             <input type="text" v-model="userInput">
-            <button @click="search()">cerca</button>
+            <button @click="pkmnCall()">cerca</button>
+            <div>
+                <ul>
+                    <li>id pokemon: {{ pokemon.id }}</li>
+                    <li>nome pokemon: {{ pokemon.name }}</li>
+                    <li>altezza pokemon: {{ pokemon.height }}</li>
+                    <li>peso pokemon: {{ pokemon.weight }}</li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -19,12 +27,19 @@ import axios from 'axios';
 
         methods: {
             pkmnCall() {
-                axios.get('https://pokeapi.co/api/v2/pokemon/',{
-                    params: {
-                    }
+
+                let pokemonName = this.userInput.toLowerCase();
+                axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`,{
+                
                 }).then((res)=>{
+                    this.pokemon = res.data
+                    console.log(this.pokemon);
                     
                 })
+                .catch((error) => {
+                    console.log('errore nella chiamata API');
+                    
+                });
             }
         }
     }
